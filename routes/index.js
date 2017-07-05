c52dff0f3505a08afac7f51b27b50f8c30f65ef4
David Kezi <davidkezi6@gmail.com>
2017-07-05 14:22:59 +0100

    added authentication

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+	  title: 'BulkSMS' });
+});
+router.get('/home', function(req, res) {
+  res.render('home', {
+	  title: 'homepage' });
 });
 
 module.exports = router;
